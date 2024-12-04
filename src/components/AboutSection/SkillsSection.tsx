2e508642ef786cd8183fe248dd05973969c247aa
LeeJaeHo <l2281@naver.com>
2024-12-05 03:40:31 +0900

    feat : "SkillSection 반응형"

--- src/components/AboutSection/SkillsSection.tsx
+++ src/components/AboutSection/SkillsSection.tsx
@@ -7,7 +7,7 @@ import AnimationComponent from "../LottieAnimation/SkillsAnimation";
 interface IconProps {
   src: string;
   alt: string;
-  label: string; // 아이콘 이름
+  label: string;
 }
 
 const Icon: React.FC<IconProps> = ({ src, alt, label }) => {
@@ -15,40 +15,39 @@ const Icon: React.FC<IconProps> = ({ src, alt, label }) => {
 
   const handleMouseEnter = () => setTooltipVisible(true);
   const handleMouseLeave = () => setTooltipVisible(false);
+  const handleTouchStart = () => setTooltipVisible(true);
+  const handleTouchEnd = () => setTooltipVisible(false);
 
   return (
     <div
-      className="flex flex-col items-center justify-center relative"
+      className="flex flex-col items-center justify-center relative group"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onTouchStart={handleTouchStart} // 터치 시작 시 hover 활성화
+      onTouchEnd={handleTouchEnd} // 터치 종료 시 hover 비활성화
     >
-      {/* 아이콘 이미지 */}
       <motion.img
         src={src}
         alt={alt}
-        className="w-12 h-12 mx-5"
+        className="w-10 h-10 sm:w-12 sm:h-12 mx-5"
         initial={{ opacity: 0, y: 10 }}
         animate={
           isTooltipVisible ? { opacity: 1, y: 0 } : { opacity: 1, y: 10 }
-        } // 마우스 오버 시 보이도록 설정
-        transition={{ duration: 0.5 }} // 애니메이션 지속 시간
+        }
+        transition={{ duration: 0.5 }}
       />
-
-      {/* 아이콘 이름 */}
       <motion.p
-        className="text-sm font-semibold mt-2 text-center text-black"
+        className="text-xs sm:text-sm font-semibold mt-2 text-center text-black"
         initial={{ opacity: 0, y: 10 }}
         animate={
           isTooltipVisible ? { opacity: 1, y: 0 } : { opacity: 1, y: 10 }
-        } // 마우스 오버 시 보이도록 설정
-        transition={{ duration: 0.5 }} // 애니메이션 지속 시간
+        }
+        transition={{ duration: 0.5 }}
       >
         {label}
       </motion.p>
-
-      {/* 툴팁 */}
       {isTooltipVisible && (
-        <div className="absolute bottom-16 bg-black text-white text-xs rounded px-2 py-1">
+        <div className="absolute bottom-16 bg-black text-white text-xs sm:text-sm rounded px-2 py-1">
           {label}
         </div>
       )}
@@ -56,14 +55,13 @@ const Icon: React.FC<IconProps> = ({ src, alt, label }) => {
   );
 };
 
-// 각 스킬 그룹에 대한 아이콘 목록 컴포넌트
 interface SkillsListProps {
   skills: { src: string; alt: string; label: string }[];
 }
 
 const SkillsList: React.FC<SkillsListProps> = ({ skills }) => (
   <motion.div
-    className="flex flex-wrap gap-4 mt-4"
+    className="flex flex-wrap justify-center gap-2 sm:gap-4 mt-4"
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     transition={{ duration: 0.3 }}
@@ -79,11 +77,10 @@ const SkillsList: React.FC<SkillsListProps> = ({ skills }) => (
   </motion.div>
 );
 
-// Bucket 컴포넌트 타입 정의
 interface BucketProps {
   id: string;
   label: string;
-  skills: { src: string; alt: string; label: string }[]; // 아이콘 목록
+  skills: { src: string; alt: string; label: string }[];
 }
 
 const Bucket: React.FC<BucketProps> = ({ id, label, skills }) => {
@@ -91,35 +88,39 @@ const Bucket: React.FC<BucketProps> = ({ id, label, skills }) => {
 
   const handleMouseEnter = () => setHovered(true);
   const handleMouseLeave = () => setHovered(false);
+  const handleTouchStart = () => setHovered(true);
+  const handleTouchEnd = () => setHovered(false);
 
   return (
     <div
-      className="bucket relative flex items-center justify-center"
+      className="bucket relative flex items-center justify-center w-80 h-80 sm:w-96 sm:h-96"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onTouchStart={handleTouchStart} // 터치 시작 시 hover 활성화
+      onTouchEnd={handleTouchEnd} // 터치 종료 시 hover 비활성화
     >
       <motion.div
         className={`flex items-center justify-center text-center transition-all duration-300 ease-in-out ${
-          isHovered ? "bg-white rounded-lg shadow-3xl" : "bg-black rounded-full"
+          isHovered ? "bg-white rounded-lg shadow-xl" : "bg-black rounded-full"
         }`}
         style={{
-          width: "300px",
-          height: "300px",
+          width: "100%",
+          height: "100%",
           borderRadius: isHovered ? "12px" : "50%",
         }}
         initial={{ borderRadius: "50%", scale: 1 }}
         animate={{ borderRadius: isHovered ? "12px" : "50%", scale: 1 }}
         transition={{ duration: 0.3 }}
       >
-        <div className="content flex flex-col items-center justify-center">
+        <div className="content flex flex-col items-center justify-center p-4">
           <motion.p
-            className={`text-2xl font-bold transition-colors duration-300 ${
+            className={`text-lg sm:text-2xl font-bold transition-colors duration-300 ${
               isHovered ? "text-black" : "text-white"
             }`}
           >
             {label}
           </motion.p>
-          {isHovered && <SkillsList skills={skills} />} {/* 아이콘 목록 표시 */}
+          {isHovered && <SkillsList skills={skills} />}
         </div>
       </motion.div>
     </div>
@@ -127,7 +128,6 @@ const Bucket: React.FC<BucketProps> = ({ id, label, skills }) => {
 };
 
 const SkillsSection: React.FC = () => {
-  // 각 스킬 그룹에 대한 아이콘 목록 정의
   const mainSkills = [
     { src: "/images/icons/Skills/html_color.svg", alt: "HTML", label: "HTML" },
     { src: "/images/icons/Skills/css.svg", alt: "CSS", label: "CSS" },
@@ -170,16 +170,8 @@ const SkillsSection: React.FC = () => {
       alt: "Bootstrap",
       label: "Bootstrap",
     },
-    {
-      src: "/images/icons/Skills/c++.svg",
-      alt: "C++",
-      label: "C++",
-    },
-    {
-      src: "/images/icons/Skills/python.svg",
-      alt: "Python",
-      label: "Python",
-    },
+    { src: "/images/icons/Skills/c++.svg", alt: "C++", label: "C++" },
+    { src: "/images/icons/Skills/python.svg", alt: "Python", label: "Python" },
   ];
 
   const studyingSkills = [
@@ -191,26 +183,14 @@ const SkillsSection: React.FC = () => {
   ];
 
   const cooperationSkills = [
-    {
-      src: "/images/icons/Skills/github.svg",
-      alt: "Github",
-      label: "Github",
-    },
-    {
-      src: "/images/icons/Skills/figma.svg",
-      alt: "Figma",
-      label: "Figma",
-    },
+    { src: "/images/icons/Skills/github.svg", alt: "Github", label: "Github" },
+    { src: "/images/icons/Skills/figma.svg", alt: "Figma", label: "Figma" },
     {
       src: "/images/icons/Skills/discord.svg",
       alt: "Discord",
       label: "Discord",
     },
-    {
-      src: "/images/icons/Skills/notion.svg",
-      alt: "Notion",
-      label: "Notion",
-    },
+    { src: "/images/icons/Skills/notion.svg", alt: "Notion", label: "Notion" },
   ];
 
   return (
@@ -219,13 +199,13 @@ const SkillsSection: React.FC = () => {
       className="relative min-h-screen flex flex-col items-center justify-center bg-white p-6 md:p-10"
     >
       {/* 애니메이션 컴포넌트 */}
-      <div className="absolute top-12 right-12">
+      <div className="absolute top-12 right-12 hidden lg:block">
         <AnimationComponent />
       </div>
-      <h2 className="text-3xl md:text-6xl font-bold text-black mb-20">
+      <h2 className="text-2xl sm:text-4xl md:text-6xl font-bold text-black mb-12 sm:mb-20">
         Skills
       </h2>
-      <div className="bucket-container flex flex-wrap gap-6 md:gap-12">
+      <div className="bucket-container flex flex-wrap justify-center gap-6 md:gap-12">
         <Bucket id="main-skills" label="Main Skills" skills={mainSkills} />
         <Bucket id="sub-skills" label="Sub Skills" skills={subSkills} />
         <Bucket id="studying" label="Studying" skills={studyingSkills} />
